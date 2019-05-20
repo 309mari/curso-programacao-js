7d5a91990fbd90fdd14a067fab7bd724943cea74
Paulo Rogério Sales Santos <paulosales@gmail.com>
2019-05-20 20:48:52 -0300

    Criando plano de exemplos e finalizando o primeiro exemplo.

--- 001-variaveis-e-tipos-de-dados.js
+++ 001-variaveis-e-tipos-de-dados.js
@@ -1,3 +1,11 @@
+/**
+ * Exemplo didático 001 - Variáveis de tipos de dados.
+ * Leia o código de exemplo, se precisar, fale com o instrutor para retirar as dúvidas.
+ * No final do código de exemplo há um exercício que você deve fazer e depois colocar o
+ * código no Github para que o professor possar corrigi-lo.
+ * Autor: Paulo Rogério Sales Santos - paulosales@gmail.com
+ */
+
 //Declarando uma variável cujo conteúdo é um número inteiro.
 var numero_inteiro = 4
 
@@ -15,3 +23,16 @@ console.log("O conteúdo da variavél com casas deximais é: " + numero_decimal)
 
 //Imprimindo na telq o conteúdo da variável "texto"
 console.log("O conteúdo da variável texto é: " + texto)
+
+
+/**
+ * ================================= 
+ *  EXERCÍCIO
+ * =================================
+ * 
+ * Faça um programa javascript que exiba da tela as informações sobre você:
+ * 
+ * Meu nome é {seu nome}
+ * Eu tenho {sua idade}
+ * 
+ */
