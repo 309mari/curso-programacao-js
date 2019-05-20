8fcd8ee89eefc75f7c1ffe8ba3d739bc6bc56769
Paulo Rogério Sales Santos <paulosales@gmail.com>
2019-05-19 21:46:16 -0300

    concluindo exemplo 001

--- 001-variaveis-e-tipos-de-dados.js
+++ 001-variaveis-e-tipos-de-dados.js
@@ -1,3 +1,17 @@
+//Declarando uma variável cujo conteúdo é um número inteiro.
 var numero_inteiro = 4
-var numero_decimal = 3.5
+
+//Declarando uma variável cujo conteúdo é um número com casas decimais
+var numero_decimal = 3.1415
+
+//Declarando uma variável cujo conteúdo é um texto
 var texto = "Não se pode confiar em um homem sem vícios"
+
+//Imprimindo na tela o conteúdo da variável "numero_inteiro"
+console.log("O cobteúdo da variável inteiro é: " + numero_inteiro)
+
+//Imprimindo na tela o conteúdo da variável "numero_decimal"
+console.log("O conteúdo da variavél com casas deximais é: " + numero_decimal)
+
+//Imprimindo na telq o conteúdo da variável "texto"
+console.log("O conteúdo da variável texto é: " + texto)
